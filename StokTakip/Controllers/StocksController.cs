4bfaa8d7b652415b8e7066af7704a898750cdb25
yasin selek <selek_55@hotmail.com>
2022-11-24 00:18:07 +0300

    açıklama satırları eklendi

--- StokTakip/Controllers/StocksController.cs
+++ StokTakip/Controllers/StocksController.cs
@@ -20,15 +20,24 @@ namespace StokTakip.Controllers
             _context = context;
         }
 
+        /*
+         * metotlar 4 çeşittir
+         * geri dönüşü olan ve olmayan
+         * parametre alan ve almayan
+         */
+
         // GET: Stocks
         // liste sayfamız
         public async Task<IActionResult> Index()
         {
+            // stoklar db den çekiyor
             var stockList = await _context.Stock.ToListAsync();
 
             // Satış Gelirini Hesaplayan Döngümüz
             foreach (var item in stockList)
             {
+                // SellingPrice null bir alandır
+                // null olunca matetiksel işlemler yapamayız o yüzden null yerine 0 değerini atıyoruz
                 item.SalesRevenue = ((item.SellingPrice ?? 0) - (item.BuyingPrice ?? 0)) * (item.Quantity ?? 0);
             }
 
@@ -39,40 +48,28 @@ namespace StokTakip.Controllers
                 totalSalesRevenue += stockList[i].SalesRevenue;
             }
 
+            // ön yüze view ile iletiyoruz
             ViewBag.TotalSalesRevenue = totalSalesRevenue;
-             
+
             return View(stockList);
         }
 
-        // GET: Stocks/Details/5
-        // Stoğun id sini alıp stoğu geri dönüyoruz
-        public async Task<IActionResult> Details(Guid? id)
-        {
-            if (id == null || _context.Stock == null)
-            {
-                return NotFound();
-            }
-
-            var stock = await _context.Stock.FirstOrDefaultAsync(m => m.Id == id);
-            if (stock == null)
-            {
-                return NotFound();
-            }
-
-            return View(stock);
-        }
-
         // GET: Stocks/Create
+        // hiç bir paremetre ve stok göndermiyoruz 
+        // create anında ön yüzden bize gelecek veriler
         public IActionResult Create()
         {
             return View();
         }
 
+        // stok imajlarını kaydetmek için yazılan metot
+        // ön yüzden alınan IFormFile nesnesini veri tabanı nesnesi olan Image nesnesine çeviriyor -> yardımcı metot
         Image? ToImage(IFormFile? image)
         {
             if (image == null)
                 return null;
 
+            // ön yüzden gelen imajı ramdan alıyoruz
             using var ms = new MemoryStream();
             image.CopyTo(ms);
 
@@ -88,22 +85,59 @@ namespace StokTakip.Controllers
         // POST: Stocks/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Create anında ön yüzden gelen verileri karşılıyoruz
+        // Stock parametresi ile karşılıyoruz Stock nesnemiz bizim veri tabanı nesnemiz
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Stock stock)
         {
-            if (ModelState.IsValid)
+            // ön yüzden gelen stok nesnemizde hata varmı kontrolü yapılır
+            if (!ModelState.IsValid)
+                // hata varsa stock nesnesi ön yüze geri gönderilir
+                return View(stock);
+
+            // hatayı bulup geri ne kadar önce döndürürsek 
+            // kod okunabilirliği ve yönetimi o kadar çok olur
+
+            // yeni bir Guid üretiyoruz
+            stock.Id = Guid.NewGuid();
+
+            //  ön yüzden alınan IFormFile nesnesini veri tabanı nesnesi olan Image nesnesine çeviriyor
+            stock.ImageData = ToImage(stock.Image);
+
+            // stoğu veri tabanına kaydediyoruz
+            _context.Add(stock);
+
+            // veri tabanına değişkenleri yazdıyoruz
+            await _context.SaveChangesAsync();
+
+            // herşey hatasız ise -> liste sayfamız gelir
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Stocks/Details/5
+        // Stoğun id sini alıp stoğu geri dönüyoruz
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Stock == null)
             {
-                stock.Id = Guid.NewGuid();
-                stock.ImageData = ToImage(stock.Image);
-                _context.Add(stock);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
+
+            // stoğun db den çekiyoruz
+            // identification yani id tekil numarasına göre
+            var stock = await _context.Stock.FirstOrDefaultAsync(m => m.Id == id);
+            if (stock == null)
+            {
+                // stok yoksa -> bulunamadı hatası dönüyoruz
+                return NotFound();
+            }
+
             return View(stock);
         }
 
         // GET: Stocks/Edit/5
+        // Stoğun id sini alıp stoğu geri dönüyoruz -> detay sayfası gibi
         public async Task<IActionResult> Edit(Guid? id)
         {
             if (id == null || _context.Stock == null)
@@ -116,12 +150,16 @@ namespace StokTakip.Controllers
             {
                 return NotFound();
             }
+
             return View(stock);
         }
 
         // POST: Stocks/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Edit anında ön yüzden gelen verileri karşılıyoruz
+        // Stock parametresi ile karşılıyoruz Stock nesnemiz bizim veri tabanı nesnemiz
+        // Create sayfalama sayfamız gibi
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Guid id, Stock stock)
@@ -131,31 +169,43 @@ namespace StokTakip.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
+                return View(stock);
+
+            // stok olup olmadığı kontrolü yapılır
+            if (!StockExists(stock.Id))
             {
-                try
-                {
-                    stock.ImageData = ToImage(stock.Image);
-                    _context.Update(stock);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StockExists(stock.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
-            return View(stock);
+
+            try
+            {
+                stock.ImageData = ToImage(stock.Image);
+
+                // stoğu veri tabanında güncelliyoruz
+                _context.Update(stock);
+
+                // veri tabanına değişkenleri yazdıyoruz
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockExists(stock.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            // herşey hatasız ise -> liste sayfamız gelir
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Stocks/Delete/5
+        // Stoğun id sini alıp stoğu geri dönüyoruz -> detay sayfası gibi
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null || _context.Stock == null)
@@ -173,8 +223,9 @@ namespace StokTakip.Controllers
         }
 
         // POST: Stocks/Delete/5
+        // Stoğun id sini alıp bu sayfada stoğu siliyoruz
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]  
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             if (_context.Stock == null)
@@ -185,18 +236,23 @@ namespace StokTakip.Controllers
             var stock = await _context.Stock.FindAsync(id);
             if (stock != null)
             {
+                // stok silme işlemi
                 _context.Stock.Remove(stock);
-            }
 
-            await _context.SaveChangesAsync();
+                // veri tabanına değişkenleri yazdıyoruz
+                await _context.SaveChangesAsync();
+            }
+             
             return RedirectToAction(nameof(Index));
         }
 
+        // stok olup olmadığı kontrolü yapılır -> yardımcı metot
         private bool StockExists(Guid id)
         {
             return _context.Stock.Any(e => e.Id == id);
         }
 
+        // herhangi bir hata olunca bu sayfaya yönlendirilir
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
